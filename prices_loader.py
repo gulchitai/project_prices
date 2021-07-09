8e464bfa418a1f55e43db80c85339690ab2d8199
gulchitai <gulchitai@bk.ru>
2021-07-09 21:26:05 +1000

    Визуальная часть загрузчика готова

--- prices_loader.py
+++ prices_loader.py
@@ -12,16 +12,13 @@ if uploaded_file is not None:
     df = read_excel(uploaded_file)
     st.dataframe(df.head(n=10))
 
-
-    add_selectbox_header = st.selectbox(
-        "Заголовок",
-        df.columns.to_list()
-    )
-    add_selectbox_price = st.selectbox(
-        "Цена",
-        df.columns.to_list()
-    )
-    first_row = st.number_input("Первая строка")
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        add_selectbox_header = st.selectbox("Заголовок",df.columns.to_list())
+    with col2:
+        add_selectbox_price = st.selectbox("Цена",df.columns.to_list())
+    with col3:
+        first_row = st.number_input("Первая строка",min_value=1, max_value=20, step=1)
     vendor = st.text_input("Поставщик")
 
     submit = st.button("Загрузить прайс")
